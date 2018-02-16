2b4b755fce053f566b24581849e45325b1ce3b5b
Anson <leungans@gmail.com>
2018-02-16 01:33:03 -0800

    write cool async await api call

--- src/App.js
+++ src/App.js
@@ -5,29 +5,27 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      data: []
+      data: [],
+      loading: true
     }
   }
 
-  componentDidMount() {
-    const KEY = process.env.REACT_APP_PETFINDER_KEY
-    const proxy = 'https://cors-anywhere.herokuapp.com/'
-    const location = '94530'
-    const target = `http://api.petfinder.com/pet.find?format=json&key=${KEY}&location=${location}`
-
-    fetch(proxy + target)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      return data;
-    })
-    .catch(error => {
-      console.log(error)
-      return error
-    })
+  async componentDidMount() {
+    this.apiPetfinder();
   }
 
+  async apiPetfinder() {
+    const KEY = process.env.REACT_APP_PETFINDER_KEY
+    // var dd;
+
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/http://api.petfinder.com/pet.find?format=json&key=${KEY}&location=94530`)
+    const data = await response.json()
+    this.setState({ data: data.petfinder.pets.pet, loading: false})
+  }
+
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         hello world
