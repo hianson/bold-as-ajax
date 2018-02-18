d1a8cb520abbfddb446633d3da5664c7f6360bb6
Anson <leungans@gmail.com>
2018-02-17 19:14:46 -0800

    goog reverse geocode api for zip code

--- src/App.js
+++ src/App.js
@@ -5,20 +5,46 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
+      location: {
+        lat: null,
+        lng: null
+      },
       data: [],
       loading: true
     }
   }
 
-  async componentDidMount() {
+  componentDidMount() {
+    // get coords
+    this.getCoordinates();
+    // reverse geocode coords into postal code
+    this.apiPostalCode();
     this.apiPetfinder();
   }
 
-  async apiPetfinder() {
-    const KEY = process.env.REACT_APP_PETFINDER_KEY
-    // var dd;
+  getCoordinates() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      var location = {lat: position.coords.latitude, lng: position.coords.longitude}
+      this.setState({ location })
+    })
+  }
 
-    const response = await fetch(`https://cors-anywhere.herokuapp.com/http://api.petfinder.com/pet.find?format=json&key=${KEY}&location=94530`)
+  async apiPostalCode() {
+    var key = process.env.REACT_APP_GOOGLE_KEY
+    var lat = this.state.location.lat
+    var lng = this.state.location.lng
+    console.log(lat, lng)
+
+    const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=${key}`)
+    const data = await response.json()
+    // console.log(data)
+    // this.setState({})
+  }
+
+  async apiPetfinder() {
+    var key = process.env.REACT_APP_PETFINDER_KEY
+
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/http://api.petfinder.com/pet.find?format=json&key=${key}&location=94530`)
     const data = await response.json()
     this.setState({ data: data.petfinder.pets.pet, loading: false})
   }
