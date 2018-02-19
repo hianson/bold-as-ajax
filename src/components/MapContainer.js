64024b266641b02a8997dc33c1958da8d8109eae
Anson <leungans@gmail.com>
2018-02-19 15:30:46 -0800

    Create basic map components

---
+++ src/components/MapContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Map from './Map'
+import {GoogleApiWrapper} from 'google-maps-react';
+
+export class Container extends React.Component {
+  render() {
+    if (!this.props.loaded) {
+      return <div>Loading...</div>
+    }
+    return (
+      <div>
+        <Map google={this.props.google}
+          />
+      </div>
+    )
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_GOOGLE_KEY
+})(Container)
